ced43c2e05abe576dd1c92b204e6be024f125827
sarahsurrender <112011947+sarahsurrender@users.noreply.github.com>
2022-09-14 01:46:46 +0000

    Sep 13, 2022, 9:46 PM

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -80,7 +80,7 @@ public class PlayerController : MonoBehaviour
             SetCountText();
         }
 
-        else if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy"))
         {
             other.gameObject.SetActive(false);
             lives = lives - 1;
